deb78ef04beb6f2375410aed5da6e3f889e43990
Polina Tyureva <polina.tyureva@devexpress.com>
2023-11-23 13:41:15 +0400

    update example

---
+++ CS/DashboardLocalizationWeb/Global.asax.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DashboardLocalizationWeb {
+    public class Global : System.Web.HttpApplication {
+
+        protected void Application_Start(object sender, EventArgs e) {
+            DevExpress.Utils.DeserializationSettings.RegisterTrustedClass(typeof(ProductSales));
+        }
+    }
+}
